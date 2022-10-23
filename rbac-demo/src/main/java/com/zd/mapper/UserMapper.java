297b97a7c533a935e8e48100a44f720018574b1f
zd <417256279@qq.com>
2022-10-23 23:18:20 +0800

    项目初始化

--- rbac-demo/src/main/java/com/zd/mapper/UserMapper.java
+++ rbac-demo/src/main/java/com/zd/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.zd.mapper;
 
+import com.zd.pojo.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 public interface UserMapper {
+    @Select("select * from rbac.user")
+    public List<User> selectAll();
+
 }
--- rbac-demo/src/main/java/com/zd/web/PermissionServlet.java
+++ rbac-demo/src/main/java/com/zd/web/PermissionServlet.java
@@ -2,4 +2,7 @@ package com.zd.web;
 
 public class PermissionServlet extends BaseServlet{
 
+
+
+
 }
